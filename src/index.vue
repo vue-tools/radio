92883140165d6e4105260628ab759ca308223695
Jerret321 <321jiangtao@gmail.com>
2016-12-25 20:35:03 +0800

    custom radio init

---
+++ src/index.vue
@@ -0,0 +1,31 @@
+<style src="./style.css"></style>
+<template src="./template.html"></template>
+<script>
+    /**
+     * @props val initial value
+     */
+    export default {
+        props: {
+            name: {
+                type: String,
+                default: ''
+            },
+            disabled: Boolean,
+            value: {},
+            val: {}
+        },
+        computed: {
+            checked(){
+                return this.model == this.val
+            },
+            model: {
+                get(){
+                    return this.value
+                },
+                set(v){
+                    this.$emit('input', v)
+                }
+            }
+        }
+    }
+</script>
